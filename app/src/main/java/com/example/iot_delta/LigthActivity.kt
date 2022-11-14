3f15e00c3561b4e1230abeb784208d57cac1b846
Diego Quirós <quirosbdiego@gmail.com>
2022-11-13 21:14:30 -0600

    Initial Commit

---
+++ app/src/main/java/com/example/iot_delta/LigthActivity.kt
@@ -0,0 +1,64 @@
+package com.example.iot_delta
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import pl.droidsonroids.gif.GifImageView
+import java.io.IOError
+import java.io.IOException
+
+class LigthActivity : AppCompatActivity(), SensorEventListener {
+
+    var sensors: Sensor? = null
+    var sensorManager: SensorManager? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ligth)
+
+        //val display: GifImageView = findViewById(R.id.display_img)
+        //display.visibility = View.INVISIBLE
+
+       // sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+       // sensors = sensorManager!!.getDefaultSensor(Sensor.TYPE_LIGHT)
+    }
+
+/*
+    override fun onResume(){
+        super.onResume()
+        sensorManager!!.registerListener(this,sensors,SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause(){
+        super.onPause()
+        sensorManager!!.unregisterListener(this)
+    }
+*/
+    var isRunning = false
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        /*try{
+            if(event!!.values[0] < 30 && isRunning == false){
+                isRunning = true
+                val display: GifImageView = findViewById(R.id.display_img)
+                display.visibility = View.VISIBLE
+            }
+            else{
+                isRunning = false
+                val display: GifImageView = findViewById(R.id.display_img)
+                display.visibility = View.INVISIBLE
+            }
+        }catch (e: IOException){
+
+        }*/
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/example/iot_delta/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.iot_delta
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.example.iot_delta.databinding.ActivityMainBinding
+import com.google.zxing.integration.android.IntentIntegrator
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnScanner.setOnClickListener{initScanner()}
+
+        val buttonClick = findViewById<Button>(R.id.btnSensorLuz)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, LigthActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    private fun initScanner() {
+        val integrator = IntentIntegrator(this)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        //integrator.setPrompt("Sigue aprendiendo en CursoKotlin.com")
+        //integrator.setTorchEnabled(true)
+        integrator.setBeepEnabled(true)
+        integrator.initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if(result != null){
+            if(result.contents == null){
+                Toast.makeText(this,"Cancelado",Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this,"El valor escaneado es: ${result.contents}",Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+
+    }
+}
